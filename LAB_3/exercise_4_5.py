e8963e6f2f6ea3f2e9df8d2706a278493b9eb60e
Jacob <jakubwiesniak102@gmail.com>
2024-03-20 21:51:42 +0100

    Update Exercise_4_5

--- LAB_3/exercise_4_5.py
+++ LAB_3/exercise_4_5.py
@@ -1,3 +1,4 @@
+import re
 """
 Niech na wejściu maszyny Turinga będzie ciąg reprezentujący listę wierzchołków zadanego grafu < G > .
 Każdy wierzchołek jest reprezentowany przez dowolne słowo generowane alfabetem {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}.
@@ -7,8 +8,26 @@ Oszacuj złożoność czasową maszyny.
 Wyjście: maszyna Turinga, oszacowanie złożoności
 """
 
+"""
+Mój pseudokod bardziej pseudo niż kod którego złożoność wynosi O(n), gdzie n to długość wejścia.
+1. Jeśli pierwszy symbol to '[', przejdź do kroku 2, w przeciwnym razie zwróć fałsz.
+2. Inicjuj listę unikalnych liczb.
+3. Dla każdego elementu w sekwencji:
+    a. Jeśli element jest liczbą, sprawdź czy nie znajduje się już w liście unikalnych liczb. Jeśli nie, dodaj do listy.
+    b. Jeśli element to kropki, sprawdź czy są dokładnie 3. Jeśli tak, przejdź do kroku 4, w przeciwnym razie zwróć fałsz.
+    c. Jeśli element to przecinek, przejdź do kroku 5.
+4. Sprawdź czy kolejny element to liczba zakończona przecinkiem. Jeśli tak, dodaj do listy unikalnych liczb i przejdź do kroku 3, w przeciwnym razie zwróć fałsz.
+5. Sprawdź czy sekwencja kończy się na '...]' i zakończ operację zwracając prawdę, w przeciwnym razie zwróć fałsz.
+
+"""
+
 def turing_machine(input, delta, list_of_input, current_state, final_state, reject_state):
     a = 0
+    if not check_input(input):
+        print(f'Wejście {input} odrzucone - niepoprawne wejście')
+        return False
+    input = turn_to_n(input)
+    print(input)
     input = list(input)
     while True:
         print(f'Aktualny stan: {current_state[0]}, znak: {input[a]} indeks głowicy: {a} input: {input}')
@@ -39,12 +58,37 @@ def turing_machine(input, delta, list_of_input, current_state, final_state, reje
             return False
 
 
+def check_input(input):
+    input = input.replace('[', '.').replace(']', '.').replace(',', '.').replace('␣', '.')
+    input = input.split('.')
+    numbers = []
+    print(input)
+    for a in input:
+        if a == '':
+            continue
+        if a.isdigit() == False:
+            return False
+        if int(a[0]) == 0:
+            return False
+        if int(a) not in numbers:
+            numbers.append(int(a))
+        else:
+            return False
+    return True
+
+def turn_to_n(input):
+    def replace_numbers(match):
+        return 'nn'
+    output_string = re.sub(r'\d+', replace_numbers, input)
+    return output_string
+
+
 # Parametry wejściowe
-list_of_states = ['q0', 'qr', 'qa', ]
+list_of_states = ['q0', 'qr', 'qa', 'q3', 'q4', 'q1', 'q2']
 list_of_input = ['n', ',', '.', '[', ']', '␣', 'n'] # gdzie n należy do {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 delta = {
     'q0': {'␣': ['qr', '', 'L'], '[': ['q1', '', 'R'], 'n': ['qr', '', 'R']},
-    'q1': {'n': ['q1', '', 'R'], '[': ['qr', '', 'R'], ']': ['qr', '', 'R'], '.': ['q3', '', 'R']},
+    'q1': {'n': ['q1', '', 'R'], '[': ['qr', '', 'R'], ']': ['qr', '', 'R'], '.': ['q3', '', 'R'], ',': ['q1', '', 'R']},
     'q3': {'.': ['q3', '', 'R'], ']': ['q4', '', 'R'], ',': ['q2', '', 'R']},
     'q4': {'␣': ['qa', '', 'L'], ']': ['qr', '', 'R'], 'n': ['qr', '', 'R'], ',': ['qr', '', 'R']},
     'q2': {'n': ['q1', '', 'R'], ',': ['qr', '', 'R']}
@@ -54,9 +98,14 @@ delta = {
 final_state = ['qa']
 reject_state = ['qr']
 
-input = '[nn...,nn,...,nn...]␣'
+
+input = '[21...,23,...,12,...,32,...,13,...]␣'
+print(check_input(input))
+
+
 print(len(input))
 current_state = ['q0']
+
 
 # Test programu
 turing_machine(input, delta, list_of_input, current_state, final_state, reject_state)
